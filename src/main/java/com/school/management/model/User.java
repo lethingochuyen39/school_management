fbe7e52abf0e1039d06208cbc8f444ea98a64dc9
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2023-06-05 21:07:53 +0700

    update

--- src/main/java/com/school/management/model/User.java
+++ src/main/java/com/school/management/model/User.java
@@ -2,9 +2,7 @@ package com.school.management.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
