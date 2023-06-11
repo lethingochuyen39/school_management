0268a9d7be6b25c251e9189448dfbe8948db8fa9
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2023-06-11 23:44:05 +0700

    update hasSchedule in ClassesRepository

--- src/main/java/com/school/management/repository/ClassesRepository.java
+++ src/main/java/com/school/management/repository/ClassesRepository.java
@@ -3,6 +3,8 @@ package com.school.management.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.school.management.model.Classes;
 
@@ -10,4 +12,8 @@ public interface ClassesRepository extends JpaRepository<Classes, Long> {
     boolean existsByName(String name);
 
     List<Classes> findByNameContainingIgnoreCase(String name);
+
+    @Query("SELECT COUNT(s) > 0 FROM Schedule s WHERE s.classes.id = :classId")
+    boolean hasSchedule(@Param("classId") Long classId);
+
 }
